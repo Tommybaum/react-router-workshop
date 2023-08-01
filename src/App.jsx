4946266e5cd4ecdd286179d48ef4b469eab3ee25
Thomas.Baumeister <thomasbaumeister3@gmail.com>
2023-08-01 13:46:32 -0500

    color components created and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-
+import Red from './components/red'
+import Blue from './components/blue'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 function App() {
 
 
@@ -9,11 +10,14 @@ function App() {
     <>
       <div id="container">
         
-        <div id="navbar"></div>
+        <div id="navbar">
+          <Link to="/blue">blue</Link>
+          <Link to="/red">red</Link>
+        </div>
         <div id="main-section">
           <Routes>
-            <Route path="/blue" element={<h1>Blue</h1>} />
-            <Route path="/red" element={<h1>Red</h1>} />
+            <Route path="/blue" element={<h1><Blue /></h1>} />
+            <Route path="/red" element={<h1><Red /></h1>} />
           </Routes>
           </div>
       </div>
